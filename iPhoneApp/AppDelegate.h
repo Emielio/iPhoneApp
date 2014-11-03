d572bdfdc8e96c097d84be66073a4ac2640ec004
Emielio <e.hoogeboom@student.tudelft.nl>
2014-11-04 00:24:39 +0100

    Initial Commit

---
+++ iPhoneApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iPhoneApp
+//
+//  Created by Emiel on 11/4/14.
+//  Copyright (c) 2014 Harvard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iPhoneApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iPhoneApp
+//
+//  Created by Emiel on 11/4/14.
+//  Copyright (c) 2014 Harvard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
